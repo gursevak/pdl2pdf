759f2c22e55abfd377443d1cb009b11fc5145c20
Irvanjit Singh <irvanjit@gmail.com>
2019-07-24 02:32:25 -0400

    updated CNAME

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import url from 'url'
-import request from 'request'
+import http from 'http'
 import cheerio from 'cheerio'
 import * as jsPDF from 'jspdf'
 import Jimp from 'jimp/es';
@@ -31,47 +31,67 @@ export default class App extends React.PureComponent {
       var pages = 1;
       var pagesLeft = 0;
       var buffer = [];
-      request({
-        uri: metaUrl,
-      }, function(error, response, body) {
-        var $ = cheerio.load(body);
-        numPages = $('td').filter(function() {
-          return $(this).text().trim() === 'Pages';
-        }).next().text();
-        pages = pagesLeft = parseInt(numPages);
-        // this.updateUserProgress();
-        for (let i = 1; i <= pages; i++) {
-          const imageUrl = `http://panjabdigilib.org/images?ID=${id}&page=${i}&pagetype=null`;
-          Jimp.read(imageUrl)
-            .then(image => {
-              // imagePromises.push(image.getBase64Async(Jimp.MIME_PNG));
-              image.getBase64(Jimp.MIME_PNG, function(err, data) {
-                if (err != null) {
-                  console.log('error');
-                } else {
-                  console.log(`converted ${i}`)
-                  buffer[i] = data;
-                  pagesLeft -= 1;
-                  self.setState({ userMessage: `Processing images... ${pagesLeft}/${pages} ਪਨੇ left`})
-                  if (pagesLeft <= 0) {
-                    self.setState({ userMessage: `Finished processing. Generating PDF...`})
-                    var doc = new jsPDF()
-                    for (let i = 1; i <= pages; i++) {
-                        doc.addImage(buffer[i], 'PNG', 15, 40, 180, 160);
-                        doc.addPage();
-                        self.setState({ userMessage: `Generating PDF... ${i}/${pages} ਪਨੇ completed`})
+      // request({
+      //   uri: metaUrl,
+      // }, function(error, response, body) {
+      //   var $ = cheerio.load(body);
+      //   numPages = $('td').filter(function() {
+      //     return $(this).text().trim() === 'Pages';
+      //   }).next().text();
+      //   pages = pagesLeft = parseInt(numPages);
+      //   // this.updateUserProgress();
+      // });
+      http.get(metaUrl, (resp) => {
+        let data = '';
+
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+          data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+          var $ = cheerio.load(data);
+          numPages = $('td').filter(function() {
+            return $(this).text().trim() === 'Pages';
+          }).next().text();
+          pages = pagesLeft = parseInt(numPages);
+          for (let i = 1; i <= pages; i++) {
+            const imageUrl = `http://panjabdigilib.org/images?ID=${id}&page=${i}&pagetype=null`;
+            Jimp.read(imageUrl)
+              .then(image => {
+                // imagePromises.push(image.getBase64Async(Jimp.MIME_PNG));
+                image.getBase64(Jimp.MIME_PNG, function(err, data) {
+                  if (err != null) {
+                    console.log('error');
+                  } else {
+                    console.log(`converted ${i}`)
+                    buffer[i] = data;
+                    pagesLeft -= 1;
+                    self.setState({ userMessage: `Processing images... ${pagesLeft}/${pages} ਪਨੇ left`})
+                    if (pagesLeft <= 0) {
+                      self.setState({ userMessage: `Finished processing. Generating PDF...`})
+                      var doc = new jsPDF()
+                      for (let i = 1; i <= pages; i++) {
+                          doc.addImage(buffer[i], 'PNG', 15, 40, 180, 160);
+                          doc.addPage();
+                          self.setState({ userMessage: `Generating PDF... ${i}/${pages} ਪਨੇ completed`})
+                      }
+                      self.setState({ userMessage: `PDF Complete. Downloading...`})
+                      doc.save(`${id}.pdf`)
+                      self.setState({ userMessage: `Download complete`})
                     }
-                    self.setState({ userMessage: `PDF Complete. Downloading...`})
-                    doc.save(`${id}.pdf`)
-                    self.setState({ userMessage: `Download complete`})
                   }
-                }
+                });
+              })
+              .catch(err => {
+                console.log("error")
               });
-            })
-            .catch(err => {
-              console.log("error")
-            });
-        }
+          }
+        });
+
+      }).on("error", (err) => {
+        console.log("Error: " + err.message);
       });
 
       this.setState({
