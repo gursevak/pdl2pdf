5d6877ce137d81f8b498a7163a6ad3b646198351
Irvanjit Singh <irvanjit@gmail.com>
2019-07-24 00:19:15 -0400

    Updates

---
+++ precache-manifest.e1e2408b4c5a9caff92543de28029484.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b231947658a2c13ed470f6e9e4ed19e5",
+    "url": "/pdl2pdf/index.html"
+  },
+  {
+    "revision": "04bd2f1d2015240c7602",
+    "url": "/pdl2pdf/static/css/main.02b0a879.chunk.css"
+  },
+  {
+    "revision": "b47117452f1180c09c46",
+    "url": "/pdl2pdf/static/js/2.bee5c271.chunk.js"
+  },
+  {
+    "revision": "04bd2f1d2015240c7602",
+    "url": "/pdl2pdf/static/js/main.cfe879b8.chunk.js"
+  },
+  {
+    "revision": "23735f5150cc7cf8b44c",
+    "url": "/pdl2pdf/static/js/runtime~main.8347f91a.js"
+  }
+]);
